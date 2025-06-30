c51ae4d85829e0d8556b1b79aa2115d2ecc8a88a
Denis Oliver Kropp <dok@directfb1.org>
2025-06-30 18:32:41 +0000

    Remove If class definition from sample_code.py

--- sample_code.py
+++ sample_code.py
@@ -1,15 +1,5 @@
 # sample_code.py
 
-class If:
-    """A simple class to represent an If condition."""
-    def __init__(self, condition):
-        self.condition = condition
-        self.default_value = 42
-
-    def __repr__(self):
-        return f"If({self.condition})"
-
-
 def calculate(a, b, c=None):
     if (isinstance(c, If)):
         return a * b if c.condition else c.default_value
