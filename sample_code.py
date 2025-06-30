e99c29802a1c9ab24fd7681bd711b2ea57fc47a9
Denis Oliver Kropp <Leichenbegatter@outlook.com>
2025-07-01 01:32:13 +0200

    cleanup

--- sample_code.py
+++ sample_code.py
@@ -1,9 +1,6 @@
 # sample_code.py
 
 def calculate(a, b, c=None):
-    if (isinstance(c, If)):
-        return a * b if c.condition else c.default_value
-
     return a + b
 
 # TODO: Add more functions and documentation.
